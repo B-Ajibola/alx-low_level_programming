58aca8a15a07d80990d8c59b3c8517d20097e716
B-Ajibola <banjiajbola15@gmail.com>
2023-02-23 10:34:07 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
